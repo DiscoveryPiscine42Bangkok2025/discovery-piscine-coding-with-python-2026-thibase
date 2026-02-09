e506ed5aeaed24aff0164710426d5aee5f9fe654
thibase <beszaza2546zaza@gmail.com>
2026-02-09 22:25:40 +0700

    Update name.py

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,4 +1,4 @@
-first_name = "Thibase"   # เปลี่ยนตรงนี้เป็นชื่อจริงภาษาอังกฤษของคุณ
-last_name = "Kanwongsa"   # เปลี่ยนตรงนี้เป็นนามสกุลจริงภาษาอังกฤษของคุณ
+first_name = "Thibase"  
+last_name = "Kanwongsa"   
 
 print(first_name, last_name,42)
