a2f35772eaee16e17d835dd666db71d7c05090de
thibase <beszaza2546zaza@gmail.com>
2026-02-09 22:48:33 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,8 @@
+# ถามชื่อ และตัดช่องว่างหน้าหลังออกด้วย .strip()
+first_name = input("Hey, what's your first name? : ").strip()
+
+# ถามนามสกุล
+last_name = input("And your last name? : ").strip()
+
+# แสดงผลทักทาย
+print("Well, pleased to meet you, " + first_name + " " + last_name + ".")
