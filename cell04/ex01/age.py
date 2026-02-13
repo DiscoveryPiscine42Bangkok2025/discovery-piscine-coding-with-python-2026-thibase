8f6bf02bffe4627596fb5f92b0b216f37e6816e1
thibase <beszaza2546zaza@gmail.com>
2026-02-13 14:09:14 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# รับค่าและแปลงเป็นตัวเลขทันที
+age = int(input("Please tell me your age: "))
+
+# แสดงผลอายุปัจจุบัน
+print("You are currently " + str(age) + " years old.")
+
+# คำนวณและแสดงผลอนาคต (ต้องแปลงกลับเป็น str เพื่อเอามาต่อกับข้อความ)
+print("In 10 years, you'll be " + str(age + 10) + " years old.")
+print("In 20 years, you'll be " + str(age + 20) + " years old.")
+print("In 30 years, you'll be " + str(age + 30) + " years old.")
