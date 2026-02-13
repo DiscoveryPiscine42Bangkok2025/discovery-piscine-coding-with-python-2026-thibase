2943907a7e14b94e317d33dfda809987c6c014d4
thibase <beszaza2546zaza@gmail.com>
2026-02-13 14:04:39 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+number = int(input())
+
+# ตรวจสอบเงื่อนไข
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
